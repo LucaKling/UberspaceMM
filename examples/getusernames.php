5264454a0eaa012816e91b966f09b883de1df220
Luca Kling <hallo@lucakling.de>
2014-02-22 00:56:42 +0100

    Changed filename and title to avoid confusion

---
+++ examples/getusernames.php
@@ -0,0 +1,33 @@
+<?php
+	Header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html lang="en" charset="utf-8">
+	<head>
+    	<title>UberspaceMM::getUsernames() example</title>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    
+    <body>
+    	<div class="container">
+            <div class="page-header">
+                <h1>UberspaceMM::getUsernames() example</h1>
+            </div>
+            <?php
+            require_once '../UberspaceMM.class.php';
+            echo '<table class="table table-bordered table-hover">';
+            echo '<tr><th>Name</th><th>Type</th><th>Forwarding destinations</th></tr>';
+            foreach(UberspaceMM::getUsernames() as $arrUser) {
+                echo '<tr><td>' . $arrUser['name'] . '</td><td>' . ($arrUser['isMailbox'] ? 'Mailbox' : 'Forwarder') . '</td><td>' . implode(', ', $arrUser['forward']) . '</td></tr>';
+            }
+            echo '</table>';
+            ?>
+        </div>
+    	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    </body>
+</html>
